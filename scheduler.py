840c2e899f52b1578e4df69daed558f22bc3d626
Thomas Hobohm <superman3275@gmail.com>
2016-08-04 15:05:03 -0500

    Fixed gitignore and refactored scheduler.py.

--- scheduler.py
+++ scheduler.py
@@ -6,11 +6,8 @@ Export UNT schedules to iCalendar format (.ics)
 import icalendar
 from datetime import datetime
 
-# Initialize Calendar
-cal = Calendar()
-
-# Add PRODID, VERSION
-# Note: PRODID inserts "name" variable. Must be provided.
-cal.add('prodid', '-//Scheduler//Schedule for ' + name + '//')
-# Note: VERSION should not be changed. RFC 5545 specifies this value should remain at 2.0.
-cal.add('version', '2.0')
+if __name__ == "__main__":
+    cal = Calendar()
+    cal.add('prodid', '-//Scheduler//Schedule for ' + name + '//')
+    # VERSION should not be changed - must remail at 2.0
+    cal.add('version', '2.0')
