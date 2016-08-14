96bf046bd40b7913327e2c2502b97de46f0776fc
Thomas Hobohm <superman3275@gmail.com>
2016-08-14 10:27:38 -0500

    Added webdriver and changed name of calendar file.

---
+++ ical.py
@@ -0,0 +1,12 @@
+"""
+File that handles interfacing with iCalendar
+"""
+
+import icalendar
+from datetime import datetime
+
+if __name__ == "__main__":
+    cal = Calendar()
+    cal.add('prodid', '-//Scheduler//Schedule for ' + name + '//')
+    # VERSION should not be changed - must remail at 2.0
+    cal.add('version', '2.0')
--- scraper.py
+++ scraper.py
@@ -1,6 +1,13 @@
 """
 Interface for web scraping
 """
+import os
+import sys
+
+from selenium import webdriver
+
+def scrape():
+    browser = webdriver.Chrome('/home/lowercase/Desktop/scheduler/chromedriver')
 
 if __name__ == "__main__":
-    pass
+    scrape()
