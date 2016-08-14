948dc7e7e6d54e4f4e41288ab014cc2e0aa53e98
Thomas Hobohm <superman3275@gmail.com>
2016-08-14 10:31:12 -0500

    Load sign in page and collect password.

--- scraper.py
+++ scraper.py
@@ -4,10 +4,22 @@ Interface for web scraping
 import os
 import sys
 
+import getpass
+
 from selenium import webdriver
 
 def scrape():
     browser = webdriver.Chrome('/home/lowercase/Desktop/scheduler/chromedriver')
+    browser.get('https://my.unt.edu/psp/papd01/EMPLOYEE/EMPL/h/?tab=NTPA_GUEST')
+    
+    euid = input('What is your EUID?')
+    password = getpass.getpass('What is your password?')
+
+    euid_field = browser.find_element_by_name('userid')
+    password_field = browser.find_element_by_name('pwd')
+
+    euid_field.send_keys(euid)
+    password_field.send_keys(password)
 
 if __name__ == "__main__":
     scrape()
