c6f144a386fd45e3b3d6daab6902a174c4422a45
Jeffrey Wang <j@mywikis.com>
2016-08-14 23:13:42 -0500

    Bug fixes and clarification

--- ical.py
+++ ical.py
@@ -6,7 +6,7 @@ from icalendar import Calendar, Event
 def make_calendar(classes):
     calendar = Calendar()
     calendar.add('prodid', '-//UNT Schedule Exporter//undercase//')
-    calendar.add('version', '1.0')
+    calendar.add('version', '2.0')
 
     for period in classes:
         event = Event()
@@ -16,7 +16,7 @@ def make_calendar(classes):
         event.add('rrule', {'freq': 'weekly'})
         calendar.add_component(event)
 
-    output = open('exported_calendar.ics', 'wb')
+    output = open('UNT_schedule.ics', 'wb')
     output.write(calendar.to_ical())
     output.close()
 
